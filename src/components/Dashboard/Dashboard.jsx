3a773e76177d8f60fb92fccbb7a5085d6c211246
Joaquín Pérez <joaquin.perez.coria@gmail.com>
2025-02-28 02:56:22 -0300

    feat(general dashboard): Trabajando en el apartado General del dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -5,7 +5,6 @@ import { General } from "./General/General";
 import "./Dashboard.css";
 
 // Componentes de cada sección
-/* const General = () => <h1 className="text-white">General</h1>; */
 const Cursos = () => <h1 className="text-white">Cursos</h1>;
 const Eventos = () => <h1 className="text-white">Eventos</h1>;
 const Certificados = () => <h1 className="text-white">Certificados</h1>;
@@ -14,29 +13,160 @@ const CerrarSesion = () => <h1 className="text-white">Cerrar Sesión</h1>;
 
 export function Dashboard() {
   const user = {
+    id: 12345,
     firstName: "Joaquín",
-    statistics: { timeConnected: 0 },
-    settings: { theme: "dark", twoStepVerification: true },
-    purchasedCourses: [{ courseId: 1, courseName: "Navegación Costera" }],
+    lastName: "Pérez",
+    email: "joaquin.perez.coria@gmail.com",
+    password: "123456",
+    phone: "098511770",
+    status: "Estudiante",
+    rank: {
+      title: "Grumete",
+      description: "Recién embarcado en la travesía del aprendizaje, aprendiendo lo básico.",
+    },
+    address: {
+      street: "Asunción 1285",
+      city: "Montevideo",
+      state: "Montevideo",
+      zipCode: "11800",
+    },
+    preferences: {
+      language: "es",
+      notifications: true,
+    },
+    statistics: {
+      eventsAttended: 0,
+      timeConnected: 0,
+    },
+    settings: {
+      theme: "dark",
+      twoStepVerification: true,
+    },
+    purchasedCourses: [
+      {
+        courseId: 1,
+        courseName: "Navegación Costera - Introducción",
+        bannerUrl: "https://wallpapercave.com/wp/wp2485810.jpg",
+        dateEnrolled: "2023-09-30",
+        status: "Completed",
+        dateCompleted: "2023-10-15",
+        certificate: {
+          credentialNumber: "CERT-12345-67890",
+          certificateLink: "https://example.com/certificates/CERT-12345-67890",
+        },
+        modulesCompleted: [
+          {
+            moduleId: 1,
+            moduleName: "Introducción",
+            lessons: [
+              {
+                lessonId: 1,
+                lessonName: "Presentación docente",
+                completed: true,
+                videoUrl: "",
+              },
+              {
+                lessonId: 2,
+                lessonName: "Metodología y objetivos",
+                completed: true,
+                videoUrl: "",
+              },
+              {
+                lessonId: 3,
+                lessonName: "¿Qué aprenderás?",
+                completed: true,
+                videoUrl: "",
+              },
+            ],
+            progress: 100,
+          },
+          {
+            moduleId: 2,
+            moduleName: "Cartas Náuticas",
+            lessons: [
+              {
+                lessonId: 1,
+                lessonName: "La carta náutica",
+                completed: true,
+                videoUrl: "",
+              },
+              {
+                lessonId: 2,
+                lessonName: "Simbología",
+                completed: true,
+                videoUrl: "",
+              },
+              {
+                lessonId: 3,
+                lessonName: "Errores comunes al interpretar la carta",
+                completed: true,
+                videoUrl: "",
+              },
+            ],
+            progress: 100,
+          },
+        ],
+        progress: 36,
+        finished: true,
+      },
+    ],
+    finishedCourses: [
+        {
+            "courseId": 1,
+            "courseName": "Navegación Costera - Avanzado",
+            "bannerUrl": "https://wallpapercave.com/wp/wp2485810.jpg",
+            "dateEnrolled": "2023-10-15",
+            "status": "Completed",
+            "dateCompleted": "2023-11-01",
+            "certificate": {
+                "credentialNumber": "CERT-54321-09876",
+                "certificateLink": "https://example.com/certificates/CERT-54321-09876"
+            }
+        }
+    ]
   };
 
   return (
     <div className="container">
       <FadeIn>
-        <div className="d-flex gap-4">
-          <aside className="text-white dashboard-menu-column col-12 col-md-3">
+        <div className="d-flex gap-0">
+          <aside className="text-white dashboard-item-build col-12 col-md-3">
             <div className="mb-5">
-              <p className="text-white mb-1">¡Bienvenido/a,</p>
-              <h1>{user.firstName}!</h1>
+              <p className="text-white mb-1">Bienvenido/a,</p>
+              <h3 className="text-orange"><strong>{user.firstName} {user.lastName}</strong></h3>
             </div>
             <ul className="dashboard-menu">
               {[
-                { to: "/dashboard/general", icon: "bi-menu-button-wide-fill", label: "General" },
-                { to: "/dashboard/cursos", icon: "bi-book-half", label: "Cursos" },
-                { to: "/dashboard/eventos", icon: "bi-calendar-event-fill", label: "Eventos" },
-                { to: "/dashboard/certificados", icon: "bi-award-fill", label: "Certificados" },
-                { to: "/dashboard/ajustes", icon: "bi-gear-fill", label: "Ajustes" },
-                { to: "/dashboard/cerrar-sesion", icon: "bi-door-open-fill", label: "Cerrar sesión" },
+                {
+                  to: "/dashboard/general",
+                  icon: "bi-menu-button-wide-fill",
+                  label: "General",
+                },
+                {
+                  to: "/dashboard/cursos",
+                  icon: "bi-book-half",
+                  label: "Cursos",
+                },
+                {
+                  to: "/dashboard/eventos",
+                  icon: "bi-calendar-event-fill",
+                  label: "Eventos",
+                },
+                {
+                  to: "/dashboard/certificados",
+                  icon: "bi-award-fill",
+                  label: "Certificados",
+                },
+                {
+                  to: "/dashboard/ajustes",
+                  icon: "bi-gear-fill",
+                  label: "Ajustes",
+                },
+                {
+                  to: "/dashboard/cerrar-sesion",
+                  icon: "bi-door-open-fill",
+                  label: "Cerrar sesión",
+                },
               ].map(({ to, icon, label }) => (
                 <NavLink
                   key={to}
@@ -51,7 +181,6 @@ export function Dashboard() {
             </ul>
           </aside>
 
-          {/* Contenido que cambia */}
           <section className="dashboard-content-column col-12 col-md-9">
             <Routes>
               <Route path="general" element={<General user={user} />} />
--- src/components/Dashboard/General/General.jsx
+++ src/components/Dashboard/General/General.jsx
@@ -1,9 +1,145 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 export function General(user) {
-    console.log(user.user);
-    return (
-        <div className="text-white">
-            <h1>¡Bienvenido/a, {user.user.firstName}!</h1>
-            <p>Estadísticas: {user.user.statistics.timeConnected} minutos conectado/a</p>
+  const course = user.user.purchasedCourses[0]; // Tomamos el primer curso
+  const progress = course.progress; // Porcentaje de progreso
+  const radius = 45; // Radio del círculo
+  const circumference = 2 * Math.PI * radius; // Circunferencia
+  const offset = circumference - (progress / 100) * circumference; // Offset dinámico
+  const rankImg = `../src/assets/ranks/${user.user.rank.title}.webp`;
+  console.log(rankImg);
+  const ranksImgs = {
+    grumete: "../src/assets/ranks/grumete.webp",
+    marinero: "../src/assets/ranks/marinero.webp",
+    cabo: "../src/assets/ranks/cabo.webp",
+    contramaestre: "../src/assets/ranks/contramaestre.webp",
+    teniente: "../src/assets/ranks/teniente.webp",
+    capitan: "../src/assets/ranks/capitan.webp",
+    almirante: "../src/assets/ranks/almirante.webp",
+  };
+
+  return (
+    <div className="d-flex justify-content-evenly flex-wrap">
+      <div className="text-white col-12 col-md-5 mb-4">
+        <h3 className="mb-3 text-orange">Panel del usuario:</h3>
+        <div className="d-flex justify-content-between">
+          <p className="mb-1">Nombre:</p>
+          <strong>
+            {user.user.firstName} {user.user.lastName}
+          </strong>
         </div>
-    );
+        <div className="d-flex justify-content-between">
+          <p className="mb-1">Email:</p>
+          <strong>{user.user.email}</strong>
+        </div>
+        <div className="d-flex justify-content-between">
+          <p className="mb-1">N° teléfono:</p>
+          <strong>{user.user.phone}</strong>
+        </div>
+        <div className="d-flex justify-content-between">
+          <p className="mb-1">Dirección:</p>
+          <strong>
+            {user.user.address.street}, {user.user.address.city}
+          </strong>
+        </div>
+        <div className="d-flex justify-content-between">
+          <p className="mb-1">Estado:</p>
+          <strong>{user.user.status}</strong>
+        </div>
+      </div>
+
+      <div className="text-white col-12 col-md-5">
+        <h3 className="mb-3 text-orange">Tu rango actual es:</h3>
+        <div className="d-flex gap-3">
+          <div className=" w-50">
+            <img
+              className="img-fluid rounded-circle"
+              src={rankImg}
+              alt={user.user.rank}
+            />
+          </div>
+          <div>
+            <h4>
+              <i class="bi bi-trophy-fill"></i> {user.user.rank.title}
+            </h4>
+            <h6>
+              <i>{user.user.rank.description}</i>
+            </h6>
+            <p>
+              Tienes <strong>{user.user.purchasedCourses.length}</strong>{" "}
+              curso/s activo/s y{" "}
+              <strong>{user.user.finishedCourses.length}</strong> curso/s
+              aprobado/s.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="text-white dashboard-item-build-general col-11">
+        <h3 className="mb-3 text-orange">¡Continúa donde quedaste!</h3>
+        <div className="d-flex gap-4 mt-3 px-2 align-items-center justify-content-between">
+          <div
+            style={{ position: "relative", width: "150px", height: "150px" }}
+          >
+            <svg width="150" height="150">
+              {/* Círculo de fondo */}
+              <circle
+                cx="75"
+                cy="75"
+                r={radius}
+                stroke="#000000"
+                strokeWidth="10"
+                fill="none"
+              ></circle>
+              {/* Círculo de progreso */}
+              <circle
+                cx="75"
+                cy="75"
+                r={radius}
+                stroke="#4CAF50"
+                strokeWidth="10"
+                fill="none"
+                strokeLinecap="round"
+                strokeDasharray={circumference}
+                strokeDashoffset={offset}
+                transform="rotate(-90 75 75)" // Rotación para que inicie desde arriba
+              ></circle>
+            </svg>
+            {/* Imagen del curso en el centro */}
+            <img
+              className="img-fluid"
+              src={course.bannerUrl}
+              alt={course.courseName}
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                width: "90px",
+                height: "90px",
+                borderRadius: "50%",
+                transform: "translate(-50%, -50%)",
+              }}
+            />
+          </div>
+          <div>
+            <h2>{course.courseName}</h2>
+            <p>
+              <i class="bi bi-bar-chart-fill"></i> Progreso: {progress}%
+            </p>
+          </div>
+          <Link
+            to={`/course/${course.courseId}`}
+            className="btn btn-warning w-25"
+          >
+            Ir al curso
+          </Link>
+        </div>
+      </div>
+
+      <div className="text-white dashboard-item-build-general col-11">
+        <h3 className="mb-3 text-orange">Estadísticas:</h3>
+      </div>
+    </div>
+  );
 }

e95a4ac2ad490a598a2f1480a60016905ed77171
Joaquin Perez <joaquin.perez.coria@gmail.com>
2025-03-10 23:35:06 -0300

    feat(Certificados): Realizado el responsive al componente de certificados

--- src/components/Dashboard/Certificados/Certificados.jsx
+++ src/components/Dashboard/Certificados/Certificados.jsx
@@ -12,18 +12,18 @@ export function Certificados({user}) {
             </div>
             <div className="d-flex flex-column gap-3">
                 {cursosAprobados.map((curso, index) => (
-                    <div key={index} className="d-flex flex-row col-12 p-4 gap-4 rounded certificate-banner" style={{ backgroundImage: `url(${curso.bannerUrl})`}}>
-                        <div className="col-3 my-auto">
-                            <img className="img-fluid rounded certificate-frontline" src="../../src/assets/certificate-placeholder.webp" alt="certificado" />
+                    <div key={index} className="d-flex flex-column flex-lg-row col-12 p-4 gap-4 rounded certificate-banner" style={{ backgroundImage: `url(${curso.bannerUrl})`}}>
+                        <div className="col-12 col-lg-3 my-auto text-center">
+                            <img className="img-fluid col-6 col-lg-12 rounded certificate-frontline" src="../../src/assets/certificate-placeholder.webp" alt="certificado" />
                         </div>
-                        <div className="col-9">
-                            <h3 className="text-orange certificate-frontline">{curso.courseName || "Curso Aprobado"}</h3>
-                            <div className="d-flex flex-row gap-5">
-                                <div>
+                        <div className="col-12 col-lg-9 d-flex d-lg-block flex-column align-items-center gap-2">
+                            <h3 className="text-orange text-center text-lg-start certificate-frontline">{curso.courseName || "Curso Aprobado"}</h3>
+                            <div className="d-flex flex-column flex-sm-row gap-2 gap-sm-5">
+                                <div className="text-center text-lg-start">
                                     <p className="m-0 text-white certificate-frontline">Fecha de finalización:</p>
                                     <strong className="text-white certificate-frontline">{curso.dateCompleted}</strong>
                                 </div>
-                                <div>
+                                <div className="text-center text-lg-start">
                                     <p className="m-0 text-white certificate-frontline">N.º de Credencial:</p>
                                     <strong className="text-white certificate-frontline">{curso.certificate.credentialNumber}</strong>
                                 </div>

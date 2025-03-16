13f72803f9836d69cf3078e7bcc24296aea7d4c7
Joaquin Perez <joaquin.perez.coria@gmail.com>
2025-03-16 00:58:12 -0300

    fix(programa-curso): Quitado el padding del listado del programa de los cursos

--- src/components/ProgramaCurso/ProgramaCurso.jsx
+++ src/components/ProgramaCurso/ProgramaCurso.jsx
@@ -1,6 +1,6 @@
 export function ProgramaCurso({ courseModules }) {
     return (
-      <ul className="d-flex flex-column gap-3">
+      <ul className="d-flex flex-column gap-3 p-0">
         {courseModules.map((module, index) => (
           <li key={index} className="d-flex gap-3">
             <p className="m-0 align-content-center">
